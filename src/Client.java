d2bf08532982ad47de4d703cc244e3c0a3ec1392
michaelkoppen <mkoppen98@hotmail.com>
2017-08-30 14:32:26 +1000

    create very basic client which sends a packet to the server.

--- src/Client.java
+++ src/Client.java
@@ -4,24 +4,20 @@ import java.net.MulticastSocket;
 import java.net.DatagramPacket;
 import java.util.Scanner;
 
-public class MulticastServer {
+public class Client {
     public static void main(String[] args) throws Exception {
         //Connect socket to inet address
         InetAddress address = InetAddress.getByName("228.5.6.7");
         MulticastSocket socket = new MulticastSocket(49321);
         socket.joinGroup(address);
 
-        //Receive packet
-        byte[] messageBuffer = new byte[1024];
-        DatagramPacket receivedPacket = new DatagramPacket(messageBuffer, 1024);
-        socket.receive(receivedPacket);
-        String userInput = new String(messageBuffer);
-        System.out.println(userInput);
-
+        //Create and send packet
+        String message = "TESTING";
+        DatagramPacket packet = new DatagramPacket(message.getBytes(), message.length(), address, 49321);
+        socket.send(packet);
+        
         //Leave inet address and close socket
         socket.leaveGroup(address);
         socket.close();
-
-
     }
 }
