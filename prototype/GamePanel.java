8fd6d44d1d324e1458b696e06d9c76240e014fe1
michaelkoppen <mkoppen98@hotmail.com>
2017-10-17 23:19:47 +1000

    Attempt first method of creating GamePanel. (Gridlayout full of individual panels).

--- prototype/GamePanel.java
+++ prototype/GamePanel.java
@@ -4,22 +4,36 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
 public class GamePanel extends JFrame{
-    DrawPanel[][] grid;
+    GridTile[][] grid;
     int gridHeight;
     int gridWidth;
 
     GamePanel(int gridHeight, int gridWidth) {
         this.gridHeight = gridHeight;
         this.gridWidth = gridWidth;
-        this.grid = new DrawPanel[gridHeight][gridWidth];
-        setLayout(new GridLayout(gridHeight, gridWidth));
+        this.grid = new GridTile[gridHeight][gridWidth];
+        JPanel gamePanel = new JPanel();
+        gamePanel.setLayout(new GridLayout(gridHeight, gridWidth));
         setTitle("TRON");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(900,900);
-        for(int i = 0; i < gridHeight * gridWidth; i++) {
-            JButton button = new JButton(String.valueOf(i));
-            add(button);
+        for (int row = 0; row < gridHeight; row++) {
+            for (int col = 0; col < gridWidth; col++) {
+                GridTile tile = new GridTile();
+                tile.putClientProperty("row", row);
+                tile.putClientProperty("col", col);
+                gamePanel.add(tile);
+                this.grid[row][col] = tile;
+            }
         }
+
+
+
+        //for(int i = 0; i < gridHeight * gridWidth; i++) {
+        //    JButton button = new JButton(String.valueOf(i));
+        //    gamePanel.add(button);
+        //}
+        add(gamePanel);
 
 
 
@@ -28,15 +42,8 @@ public class GamePanel extends JFrame{
         //Container test = getContentPane();
         //test.setLayout(new BorderLayout());
         //test.add(gamePanel, BorderLayout.CENTER);
-
-
-
-        //add(gamePanel);
     }
 
-    interface Drawable {
-        void draw(Graphics g);
-    }
 
     //void createGrid() {
     //   for (int row = 0; row < gridHeight; row++) {
@@ -45,10 +52,22 @@ public class GamePanel extends JFrame{
     //           this.grid[row][col] = panel;
     //       }
     //   }
-    //
 
+    void addCycle() {
+        if("3" == grid[3][2].getClientProperty("row") && "2" == grid[3][2].getClientProperty("col"));
+        grid[3][2].setBackground(Color.yellow);
+        //for (int row = 0; row < gridHeight; row++) {
+        //    for (int col = 0; col < gridWidth; col++) {
+        //        if("3" == grid[row][col].getClientProperty("row") && "2" == grid[row][col].getClientProperty("col"));
+        //        grid[row][col].setBackground(Color.yellow);
+        //    }
+        //}
+    }
     public static void main(String[] args) {
-        GamePanel panel = new GamePanel(500, 500);
+        GamePanel panel = new GamePanel(200, 200);
         panel.setVisible(true);
+        System.out.println(panel.grid[0][6].getClientProperty("row"));
+        System.out.println(panel.grid[0][6].getClientProperty("col"));
+        panel.addCycle();
     }
 }
