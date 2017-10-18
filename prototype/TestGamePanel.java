3c4af8da6e85f51ea04ba37d663564ed154d9af8
michaelkoppen <mkoppen98@hotmail.com>
2017-10-18 16:31:04 +1000

    implement key bindings (up, down, left and right)

--- prototype/TestGamePanel.java
+++ prototype/TestGamePanel.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.KeyStroke;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -16,18 +17,60 @@ public class TestGamePanel extends JPanel implements ActionListener {
         setDoubleBuffered(true);
         t = new Timer(7, this);
         t.start();
-
+        InputMap inputMap = getInputMap(JPanel.WHEN_IN_FOCUSED_WINDOW);
+        ActionMap actionMap = getActionMap();
+        inputMap.put(KeyStroke.getKeyStroke("UP"), "upAction");
+        actionMap.put("upAction", upAction);
+        inputMap.put(KeyStroke.getKeyStroke("DOWN"), "downAction");
+        actionMap.put("downAction", downAction);
+        inputMap.put(KeyStroke.getKeyStroke("LEFT"), "leftAction");
+        actionMap.put("leftAction", leftAction);
+        inputMap.put(KeyStroke.getKeyStroke("RIGHT"), "rightAction");
+        actionMap.put("rightAction", rightAction);
 
     }
 
     @Override
     public void paintComponent(Graphics g) {
-        super.paintComponent(g);
+        //super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.fillRect(x, y, 10, 10);
-        myComponent.getInputMap().put(KeyStroke.getKeyStroke("DOWN"), moveDown());
+        g2d.setColor(Color.cyan);
+        g2d.fillRect(x, y, 50, 50);
+        //myComponent.getInputMap().put(KeyStroke.getKeyStroke("DOWN"), moveDown());
         //myComponent.getActionMap().put("myAction", moveDown());
     }
+
+    Action upAction = new AbstractAction() {
+        //@Override
+        public void actionPerformed(ActionEvent e) {
+            xVelocity = 0;
+            yVelocity = -1;
+        }
+    };
+
+    Action downAction = new AbstractAction() {
+        //@Override
+        public void actionPerformed(ActionEvent e) {
+            xVelocity = 0;
+            yVelocity = 1;
+        }
+    };
+
+    Action leftAction = new AbstractAction() {
+        //@Override
+        public void actionPerformed(ActionEvent e) {
+            xVelocity = -1;
+            yVelocity = 0;
+        }
+    };
+
+    Action rightAction = new AbstractAction() {
+        //@Override
+        public void actionPerformed(ActionEvent e) {
+            xVelocity = 1;
+            yVelocity = 0;
+        }
+    };
 
     void moveUp() {
         xVelocity = 0;
