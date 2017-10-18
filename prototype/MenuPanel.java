4971627ba7861d05aedef942f8ae033932dbb01d
michaelkoppen <mkoppen98@hotmail.com>
2017-10-19 00:30:06 +1000

    create join game button template

--- prototype/MenuPanel.java
+++ prototype/MenuPanel.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MenuPanel extends JPanel {
 
@@ -9,8 +11,23 @@ public class MenuPanel extends JPanel {
     public MenuPanel() {
         setLayout(new FlowLayout());
         setVisible(true);
-        JButton joinGame = new JButton();
-        joinGame.setText("Join Game");
+        JButton joinGame = new JButton("Join Game");
+        joinGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame addUserFrame = new JFrame();
+                addUserFrame.setResizable(false);
+                addUserFrame.setVisible(true);
+                addUserFrame.setSize(100,100);
+                JPanel addUserPanel = new JPanel();
+                addUserPanel.setLayout(new FlowLayout());
+                JTextField addUserText = new JTextField(10);
+                JButton addUserButton = new JButton("GO");
+                addUserFrame.add(addUserPanel);
+                addUserPanel.add(addUserText);
+                addUserPanel.add(addUserButton);
+            }
+        });
         add(joinGame);
     }
 
